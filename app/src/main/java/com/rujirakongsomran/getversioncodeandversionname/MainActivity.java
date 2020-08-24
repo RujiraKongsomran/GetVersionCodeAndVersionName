193d30eac6ffeec29f205a389f469508d86094e9
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-08-24 17:40:08 +0700

    Write JAVA Code

--- app/src/main/java/com/rujirakongsomran/getversioncodeandversionname/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/getversioncodeandversionname/MainActivity.java
@@ -1,14 +1,42 @@
 package com.rujirakongsomran.getversioncodeandversionname;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.rujirakongsomran.getversioncodeandversionname.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        PackageInfo pinfo = null;
+        try {
+            pinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        int versionNumber = pinfo.versionCode;
+        String versionName = pinfo.versionName;
+
+        binding.tvVersionCode.setText(String.valueOf(versionNumber));
+        binding.tvVersionName.setText(versionName);
+
+
+
+
     }
 }
